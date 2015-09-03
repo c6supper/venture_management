98c4137a7076263ae40f98b4c02b8b8b58938799
c6supper@hotmail.com <c6supper@hotmail.com>
2015-09-03 21:27:25 +0800

    add for ext.net

--- project/ventureManagement/ventureManagement.web/Properties/AssemblyInfo.cs
+++ project/ventureManagement/ventureManagement.web/Properties/AssemblyInfo.cs
@@ -2,34 +2,14 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// 有关程序集的常规信息通过下列特性集
-// 控制。更改这些特性值可修改
-// 与程序集关联的信息。
-[assembly: AssemblyTitle("ventureManagement.web")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Ext.Net.MVC.Examples")]
+[assembly: AssemblyDescription("Ext.Net.MVC.Examples")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ventureManagement.web")]
-[assembly: AssemblyCopyright("版权所有(C) Microsoft 2015")]
+[assembly: AssemblyCompany("Ext.NET, Inc.")]
+[assembly: AssemblyProduct("Ext.Net.MVC.Examples")]
+[assembly: AssemblyCopyright("Copyright © 2007-2013 Ext.NET, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// 将 ComVisible 设置为 false 会使此程序集中的类型
-// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的某个类型，
-// 请针对该类型将 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
-
-// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("ff4bfd2b-57e3-4bc0-8fa4-c044af237839")]
-
-// 程序集的版本信息由下列四个值组成:
-//
-//      主版本
-//      次版本
-//      内部版本号
-//      修订号
-//
-// 可以指定所有这些值，也可以使用“修订号”和“内部版本号”的默认值，
-// 方法是按如下所示使用“*”:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("0ad1c222-13cb-4c0b-bb33-2ca35f5bbce7")]
+[assembly: AssemblyVersion("2.2.0.*")]
