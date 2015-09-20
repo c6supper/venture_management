4f858faa662f12c5d3ea40f43009f93363124142
c6supper@hotmail.com <c6supper@hotmail.com>
2015-09-21 01:27:30 +0800

    daily commit

---
+++ project/ventureManagement/ventureManagement.web/Areas/Member/Controllers/MemberBaseController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ext.Net;
+using VentureManagement.BLL;
+using VentureManagement.IBLL;
+using VentureManagement.Models;
+using VentureManagement.Web.Controllers;
+// ReSharper disable PrivateFieldCanBeConvertedToLocalVariable
+// ReSharper disable InconsistentNaming
+
+namespace VentureManagement.Web.Areas.Member.Controllers
+{
+    public class MemberBaseController : BaseController
+    {
+        //
+        // GET: /Member/MemberBase/
+
+        protected readonly InterfaceUserService _userService;
+        protected readonly InterfaceOrganizationService _orgSerivce = new OrganizationService();
+        protected readonly InterfaceRoleService _roleSerivce = new RoleService();
+        protected readonly InterfaceOrganizationRelationService _orgrService = new OrganizationRelationService();
+        protected readonly InterfaceRoleRelationService _userRoleRelationService = new UserRoleRelationService();
+        protected readonly InterfaceUserOrganizationRelationService _userOrgRelationService = new UserOrganizationRelationService();
+
+        protected MemberBaseController()
+        {
+            var currentOrgList = System.Web.HttpContext.Current.Session["currentOrgList"] as List<int>;
+
+            _userService = new UserService(currentOrgList);
+        }
+
+    }
+}
---
+++ project/ventureManagement/ventureManagement.web/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VentureManagement.Models;
+
+namespace VentureManagement.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        //
+        // GET: /Base/
+
+        // ReSharper disable once InconsistentNaming
+        protected User _currentUser = null;
+        // ReSharper disable once InconsistentNaming
+        protected List<int> _currentOrgList = null;
+
+        public BaseController()
+        {
+            _currentUser = System.Web.HttpContext.Current.Session["User"] as User;
+            _currentOrgList = System.Web.HttpContext.Current.Session["currentOrgList"] as List<int>;
+        }
+
+    }
+}
