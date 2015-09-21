239eb2f36536cc08b2f0d9a4b2781b5f01e88534
c6supper@hotmail.com <c6supper@hotmail.com>
2015-09-22 00:15:25 +0800

    daily update

---
+++ project/ventureManagement/ventureManagement.web/Areas/Threat/Controllers/ThreatCorrectionTemplateController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VentureManagement.Models;
+
+namespace VentureManagement.Web.Areas.Threat.Controllers
+{
+    public class ThreatCorrectionTemplateController : Controller
+    {
+        //
+        // GET: /Threat/ThreatCorrectionTemplate/
+        // ReSharper disable once InconsistentNaming
+        private const string _template = "~/Areas/Threat/Content/CorrectionTemplate.xml";
+        private ThreatCorrection _correctionTemplate = null;
+
+        public ThreatCorrectionTemplateController()
+        {
+            _correctionTemplate = ThreatCorrection.Deserialize(System.Web.HttpContext.Current.Server.MapPath(_template));
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
