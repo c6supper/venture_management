1e665a22d89600f140fd45ee5adf9eea81dd1cde
yangjiyin <250156915@qq.com>
2015-09-21 21:21:24 +0800

    add files.

---
+++ project/ventureManagement/ventureManagement.web/Areas/Project/Controllers/ProjectBaseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VentureManagement.BLL;
+using VentureManagement.IBLL;
+using VentureManagement.Web.Controllers;
+
+namespace VentureManagement.Web.Areas.Project.Controllers
+{
+    public class ProjectBaseController : BaseController
+    {
+        //
+        // GET: /Project/ProjectBase/
+        protected readonly InterfaceProjectService _projectService;
+        protected readonly InterfaceProjectRelationService _projectRelationService;
+
+        protected ProjectBaseController()
+        {
+            _projectService = new ProjectService(_currentOrgList);
+            _projectRelationService = new ProjectRelationService(_currentOrgList);
+        }
+    }
+}
---
+++ project/ventureManagement/ventureManagement.web/Areas/Threat/Controllers/ThreatBaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VentureManagement.BLL;
+using VentureManagement.IBLL;
+using VentureManagement.Web.Controllers;
+
+namespace VentureManagement.Web.Areas.Threat.Controllers
+{
+    public class ThreatBaseController : BaseController
+    {
+        //
+        // GET: /Threat/ThreatBase/
+        protected readonly InterfaceThreatCaseService _threatCaseService;
+
+        protected ThreatBaseController()
+        {
+            _threatCaseService = new ThreatCaseService(_currentOrgList);
+        }
+    }
+}
