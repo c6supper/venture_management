5639036cfa52c6cd55df64b68f11f3f1caed1721
c6supper <c6supper@hotmail.com>
2015-10-26 18:01:03 +0800

    daily commit

--- project/ventureManagement/ventureManagement.web/Areas/Threat/Controllers/CreateThreatCaseController.cs
+++ project/ventureManagement/ventureManagement.web/Areas/Threat/Controllers/CreateThreatCaseController.cs
@@ -145,67 +145,19 @@ namespace VentureManagement.Web.Areas.Threat.Controllers
             return this.Direct();
         }
 
-        //[ValidateInput(true)]
-        //public ActionResult CreateThreatCase(List<FieldsGroupModel> groups1, List<FieldsGroupModel> groups2)
-        //{
-        //    StringBuilder sb = new StringBuilder(255);
 
-        //    sb.Append("<h1>Checked Items</h1>");
-        //    sb.Append("<h2>CheckboxGroups</h2>");
-        //    sb.Append("<blockquote>");
 
-        //    groups1.ForEach(delegate(FieldsGroupModel group)
-        //    {
-        //        int count = 0;
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase[] fileToUpload)
+        {
 
-        //        group.CheckedItems.ForEach(delegate(CheckedFieldModel checkbox)
-        //        {
-        //            if (count == 0)
-        //            {
-        //                sb.AppendFormat("<h3>{0}</h3>", group.FieldLabel);
-        //                sb.Append("<blockquote>");
-        //            }
-        //            sb.AppendFormat("{0}<br />", checkbox.BoxLabel);
-        //            count++;
-        //        });
+            foreach (var file in fileToUpload)
+            {
+                string path = System.IO.Path.Combine(Server.MapPath("~/App_Data"), System.IO.Path.GetFileName(file.FileName));
+                file.SaveAs(path);
+            }
 
-        //        if (count > 0)
-        //        {
-        //            sb.Append("</blockquote>");
-        //        }
-        //    });
-
-        //    sb.Append("</blockquote>");
-
-        //    sb.Append("<h2>RadioGroups</h2>");
-        //    sb.Append("<blockquote>");
-
-        //    groups2.ForEach(delegate(FieldsGroupModel group)
-        //    {
-        //        int count = 0;
-
-        //        group.CheckedItems.ForEach(delegate(CheckedFieldModel radio)
-        //        {
-        //            if (count == 0)
-        //            {
-        //                sb.AppendFormat("<h3>{0}</h3>", group.FieldLabel);
-        //                sb.Append("<blockquote>");
-        //            }
-        //            sb.AppendFormat("{0}<br />", radio.BoxLabel);
-        //            count++;
-        //        });
-
-        //        if (count > 0)
-        //        {
-        //            sb.Append("</blockquote>");
-        //        }
-        //    });
-
-        //    sb.Append("</blockquote>");
-
-        //    this.GetCmp<Label>("Label1"). = sb.ToString();
-
-        //    return this.Direct();
-        //}
+            return RedirectToAction("Index");
+        }
     }
 }
